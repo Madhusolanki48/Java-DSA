3c60b0cfae6777bf2eb04585de8e60adf08ea767
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-01-17 23:58:27 +0530

    optimised code-prime numb

--- 1.BasicsJava/Java6_9IsPrimeOrNot.java
+++ 1.BasicsJava/Java6_9IsPrimeOrNot.java
@@ -1,23 +1,35 @@
 public class Java6_9IsPrimeOrNot {
     //only for n>=2
-    public static boolean isPrime(int n){
-        boolean isPrime=true;
+//    public static boolean isPrime(int n){
+//        boolean isPrime=true;
+//
+//        //corner cases
+//        //2
+//        if(n==2){
+//            return true;
+//        }
+//        for(int i=2;i<=n-1;i++){ //completely dividing
+//            if(n%i==0){
+//                isPrime=false;   // return false
+//                break;   //or return isPrime;
+//            }
+//        }
+//        return isPrime;
+//    }
 
-        //corner cases
-        //2
+    public static boolean isPrime(int n){
         if(n==2){
             return true;
         }
-        for(int i=2;i<=n-1;i++){ //completely dividing
-            if(n%i==0){
-                isPrime=false;   // return false
-                break;   //or return isPrime;
+        for(int i=2;i<=Math.sqrt(n);i++){
+            if(n % i ==0){
+                return false;
             }
         }
-        return isPrime;
+        return true;
     }
 
     public static void main(String[] args) {
-        System.out.println(isPrime(12));
+        System.out.println(isPrime(7));
     }
 }
