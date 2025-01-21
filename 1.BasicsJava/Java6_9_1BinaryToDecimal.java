ecf0955e1bf41456730f129d93377a29c5f96bf6
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-01-21 23:01:50 +0530

    binary to decimal

---
+++ 1.BasicsJava/Java6_9_1BinaryToDecimal.java
@@ -0,0 +1,19 @@
+public class Java6_9_1BinaryToDecimal {
+    public static void binaryToDecimal(int binNum) {
+        int myNum = binNum;
+        int pow = 0;
+        int decNum = 0;
+        while (binNum > 0) {
+            int lastDigit = binNum % 10;
+            decNum = decNum + (lastDigit * (int) Math.pow(2, pow));
+            pow++;
+            binNum = binNum / 10;
+        }
+        System.out.println("Decimal of " + myNum + " = " + decNum);
+    }
+
+    public static void main(String[] args) {
+        binaryToDecimal(101);
+    }
+
+}
