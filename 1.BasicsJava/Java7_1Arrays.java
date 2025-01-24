0d52f6846d407637c589c7820d9d2fd31e1f6033
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-01-24 23:24:49 +0530

    arrays

---
+++ 1.BasicsJava/Java7_1Arrays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Java7_1Arrays {
+    public static void main(String[] args) {
+        int marks[]=new int[10];
+
+        int numbers[]={1,2,3};
+
+        int moreNumbers[]={4,5,6};
+
+        String fruits[]={"Apple","Mango","Orange"};
+
+    }
+}
