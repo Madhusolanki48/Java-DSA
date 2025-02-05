5261392b3a3df3682bebf57855236069860ec3f0
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-02-05 23:48:25 +0530

    palindromic string

--- 1.BasicsJava/Java9_6StringConcatenation.java
+++
@@ -1,2 +0,0 @@
-public class Java9_6StringConcatenation {
-}
---
+++ 1.BasicsJava/Java9_7StringMethod.java
@@ -0,0 +1,14 @@
+public class Java9_7StringMethod {
+    public static void printLetters(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i)+" ");
+        }
+    }
+    public static void main(String[] args) {
+        String str = "PROGRAMMING";
+        System.out.println(str.charAt(2));
+        printLetters(str);
+
+    }
+
+}
