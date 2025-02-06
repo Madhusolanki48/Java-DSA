0a12f270d442b1a6ffc938ca851df6495a5659cd
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-02-06 23:10:23 +0530

    largest string

---
+++ 1.BasicsJava/Java9_9_2LargestString.java
@@ -0,0 +1,19 @@
+public class Java9_9_2LargestString {
+    //For a given set of Strings, print the largest string.
+    //Lexicographical order
+
+    //compareTo Function
+
+    public static void main(String[] args) {
+        String fruits[]={"appple","mango","banana"};
+        String largest=fruits[0];
+        for(int i=0;i<fruits.length;i++){
+            if(largest.compareTo(fruits[i])<0){
+                largest=fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
+
+               //Time Complexity=O(n*x)
