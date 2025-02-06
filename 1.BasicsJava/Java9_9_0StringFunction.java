7ce41824e5225949cc8152bd56fa77a5864319ca
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-02-06 23:03:20 +0530

    string func

---
+++ 1.BasicsJava/Java9_9_0StringFunction.java
@@ -0,0 +1,26 @@
+public class Java9_9_0StringFunction {
+    public static void main(String[] args) {
+        String s1="Tony";
+        String s2="Tony";
+        String s3= new String("Tony");
+
+        if(s1==s2){
+            System.out.println("Strings s1 & s2 are equal");
+        }
+        else{
+            System.out.println("Strings s1 & s2 are not equal");
+        }
+        if(s1==s3){
+            System.out.println("Strings s1 & s3 are equal");
+        }
+        else{
+            System.out.println("Strings s1 & s3 are not equal");
+        }
+        if(s1.equals(s3)){
+            System.out.println("Strings s1 & s3 are equal");
+        }
+        else{
+            System.out.println("Strings s1 & s3 are not equal");
+        }
+    }
+}
