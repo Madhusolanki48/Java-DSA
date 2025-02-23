c62b099434edfd71ed67e87cc2acaa5862332f56
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-02-23 23:59:29 +0530

    constructor types

---
+++ 12_Oops/Java4_ConstructorTypes.java
@@ -0,0 +1,24 @@
+public class Java4_ConstructorTypes {
+    public static void main(String[] args) {
+        Student s1=new Student();
+        Student s2=new Student("Madhu");
+        Student s3=new Student(123);
+
+    }   
+    }
+    class Student{
+        String name;
+        int roll;
+
+        Student(){ //Non-Parameterized
+            System.out.println("const is called...");
+        }
+        Student(String name){ //Parameterized
+            this.name=name;
+        }
+        Student(int roll){
+            this.roll=roll;
+        }
+    }
+    
+
