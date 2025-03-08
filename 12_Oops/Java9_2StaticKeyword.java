92a5dcc29eb3b168855c251ec13bd0cef52c24ae
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-03-08 18:48:20 +0530

    super keyword

---
+++ 12_Oops/Java9_2StaticKeyword.java
@@ -0,0 +1,20 @@
+public class Java9_2StaticKeyword {
+    public static void main(String[] args) {
+        Horse h=new Horse();
+        System.out.println(h.color);
+    }
+}
+class Animal{
+    String color;
+    Animal(){
+        System.out.println("Animal constructor is called");
+    }
+}
+class Horse extends Animal{
+    Horse(){
+        super();
+        super.color="brown";
+        System.out.println("Horse Constructor is called");
+    }
+
+}
