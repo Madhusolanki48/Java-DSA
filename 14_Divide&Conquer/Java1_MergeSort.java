073dbdc14b5ae79949a3190d8c8863ffbc817fc5
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-03-27 23:45:27 +0530

    merge sort comp

--- 14_Divide&Conquer/Java1_MergeSort.java
+++ 14_Divide&Conquer/Java1_MergeSort.java
@@ -21,7 +21,44 @@ public class Java1_MergeSort {
         merge(arr,si,mid,ei);
     }
     public static void merge(int arr[],int si,int mid,int ei){
-        //left(0,3)=4 right
+        //left(0 to 3)=4 right(4 to 6)=3  -->6-0+1=7
+        int temp[]=new int[ei-si+1];
+        int i=si; //iterator for left part
+        int j=mid+1; //iterator for right part
+        int k=0; //iterator for temp arr
+
+        while(i<=mid && j<=ei){
+            if(arr[i]<arr[j]){
+                temp[k]=arr[i];
+                i++;
+                
+            }
+            else{
+                temp[k]=arr[j];
+                j++;
+                
+            }
+            k++;
+        }
+        //left part
+        while(i<=mid){
+            temp[k++]=arr[i++];
+        }
+        //right part
+        while(j <= ei){
+            temp[k++]=arr[j++];
+        }
+        //copy temp to original arr
+        for(k=0,i=si; k<temp.length;k++,i++){
+            arr[i]=temp[k];
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[]={6,3,9,5,2,8};
+        mergeSort(arr, 0, arr.length-1);
+        printArr(arr);
+        
     }
     
 }
