bab3d1bfc92a06750505a3e66121627ad4137018
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-02 23:59:29 +0530

    sudoku

---
+++ 15_Backtracking/Java4_Sudoku.java
@@ -0,0 +1,22 @@
+public class Java4_Sudoku {
+    public static boolean sudokuSolver(int sudoku[][],int row,int col){
+        //base case
+
+        //recursion
+        int nextrow=row,nextCol=col+!;
+
+
+        
+    }
+    public static void main(String[] args) {
+        int sudoku[][]={{0,0,8,0,0,0,0,0,0},
+                        {4,9,0,1,5,7,0,0,2},
+                        {0,0,3,0,0,4,1,9,0},
+                        {1,8,5,0,6,0,0,6,0},
+                        {0,0,0,0,2,0,0,6,0},
+                        {9,6,0,4,0,5,3,0,0},
+                        {0,3,0,0,7,2,0,0,4},
+                        {0,4,9,0,3,0,0,5,7},
+                        {8,2,7,0,0,9,0,1,3}};
+    }   
+}
