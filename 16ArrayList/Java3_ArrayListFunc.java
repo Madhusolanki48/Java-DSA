9df1ed259f2b0be47f5b43c6650e7f5d61b0b7c2
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-08 23:56:54 +0530

    arraylist basics

---
+++ 16ArrayList/Java3_ArrayListFunc.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Java3_ArrayListFunc {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+     list.add(1);
+     list.add(2);
+     list.add(3);
+     list.add(4);
+
+     System.out.println(list.size());
+
+     //print the arraylist
+     for(int i=0;i<list.size();i++){
+        System.out.print(list.get(i)+" ");
+     }
+     System.out.println();
+    }
+}
