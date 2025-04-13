e171deed42ac69dca206e4e4281f3df27905254c
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-13 23:15:29 +0530

    two pointer approach

--- 16ArrayList/Java9_ContainerWithMostWater.java
+++ 16ArrayList/Java9_ContainerWithMostWater.java
@@ -4,19 +4,41 @@ public class Java9_ContainerWithMostWater {
     // For a given n lines on x-axis, use 2 lines to form a container such
     // that it holds maximum water.
 
-    public static int storeWater(ArrayList<Integer>height){
+    // public static int storeWater(ArrayList<Integer>height){
+    //     int maxWater=0;
+    //     //brute force
+    //     for(int i=0;i<height.size();i++){
+    //         for(int j=i+1;j<height.size();j++){
+    //             int ht=Math.min(height.get(i),height.get(j));
+    //             int width=j-i;
+    //             int currWater=ht*width;
+    //             maxWater=Math.max(maxWater,currWater);
+    //         }
+    //     }
+    //     return maxWater;
+
+    // }
+
+    // Two- Pointer Approach
+
+    public static int storeWater(ArrayList<Integer> height){
         int maxWater=0;
-        //brute force
-        for(int i=0;i<height.size();i++){
-            for(int j=i+1;j<height.size();j++){
-                int ht=Math.min(height.get(i),height.get(j));
-                int width=j-i;
-                int currWater=ht*width;
-                maxWater=Math.max(maxWater,currWater);
+        int lp=0, rp=height.size()-1;
+        while(lp<rp){
+            //calculate water area
+            int ht=Math.min(height.get(lp),height.get(rp));
+            int width=rp-lp;
+            int currWater=ht*width;
+            maxWater=Math.max(maxWater,currWater);
+
+            //update pointer
+            if(height.get(lp)<height.get(rp)){
+                lp++;
+            }else{
+                rp--;
             }
         }
         return maxWater;
-
     }
 
     public static void main(String[] args) {
