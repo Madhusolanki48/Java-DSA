931ecdc8c60c447f2e588ce59672cf71c9b18418
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-14 23:55:18 +0530

    pair sum1 two pointer

--- 16ArrayList/Java9_1PairSum1.java
+++ 16ArrayList/Java9_1PairSum1.java
@@ -3,16 +3,38 @@ import java.util.ArrayList;
 public class Java9_1PairSum1 {
 
     //Brute Force Approach
+    // public static boolean pairSum1(ArrayList<Integer>list,int target){
+    //     for(int i=0;i<list.size();i++){
+    //         for(int j=i+1;j<list.size();j++){
+    //             if(list.get(i)+list.get(j)==target){
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false;
+    // }
+
+    // 2 Pointer Approach
+
     public static boolean pairSum1(ArrayList<Integer>list,int target){
-        for(int i=0;i<list.size();i++){
-            for(int j=i+1;j<list.size();j++){
-                if(list.get(i)+list.get(j)==target){
-                    return true;
-                }
+        int lp=0;
+        int rp=list.size()-1;
+        while(lp!=rp){
+            //case1
+            if(list.get(lp)+list.get(rp)==target){
+                return true;
+            }
+            //case2
+            if(list.get(lp)+list.get(rp)<target){
+                lp++;
+            }else{
+                //case 3
+                rp--;
             }
         }
         return false;
     }
+
     public static void main(String[] args) {
         ArrayList<Integer> list=new ArrayList<>();
         //1,2,3,4,5,6
