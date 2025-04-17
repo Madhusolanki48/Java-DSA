862b7dda860d1b9cd29b8d7557e8a6c71273cf01
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-17 22:14:37 +0530

    most freq follow prob

--- 16ArrayList/Java9_3ArraylistExercise.java
+++ 16ArrayList/Java9_3ArraylistExercise.java
@@ -25,6 +25,35 @@ public class Java9_3ArraylistExercise {
         }
         return inc || dec;
      }
+
+     /*Question 3: Most Frequent Number following Key (EASY)
+       You are given an integer ArrayList nums. You are also given an integer key, which is present in nums.
+       For every unique integer target in nums, count the number of times target immediately follows an occurrence of key in nums.
+       In other words, count the number of indices i such that:
+       0 <= i <= nums.size() - 2,
+       nums.get(i) == key, and
+       nums.get(i+1) == target.
+       Return the target with the maximum count.
+       (Assumption: The target with maximum count is unique.) 
+      */
+
+      public int mostFrequent(ArrayList<Integer>nums,int key){
+        int[] result=new int[1000];
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums.get(i)==key){
+                result[nums.get(i+1)-1]++;
+            }
+        }
+        int max=Integer.MIN_VALUE;
+        int ans=0;
+        for(int i=0;i<1000;i++){
+            if(result[i]>max){
+                max=result[i];
+                ans=i+1;
+            }
+        }
+        return ans;
+      }
      public static void main(String[] args) {
         ArrayList<Integer> list=new ArrayList<>();
         
