156fd3a6a5813321a115c5ccf5220d888f55e1f9
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-20 23:40:15 +0530

    beautiful al divide n conquer approach

--- 16ArrayList/Java9_3ArraylistExercise.java
+++ 16ArrayList/Java9_3ArraylistExercise.java
@@ -120,6 +120,21 @@ public class Java9_3ArraylistExercise {
         return ans;
     }
 
+    //Divide and conquer approach
+    public ArrayList<Integer> beautifulArray2(int n){
+        ArrayList<Integer> res=new ArrayList<>();
+        divideConquer(1,1,res,n);
+        return res;
+    }
+    private void divideConquer(int start,int increment,ArrayList<Integer>res,int n){
+        if(start+increment>n){
+            res.add(start);
+            return;
+        }
+        divideConquer(start, 2*increment, res, n);
+        divideConquer(start+increment, 2*increment, res, n);
+    }
+
 
 
      public static void main(String[] args) {
