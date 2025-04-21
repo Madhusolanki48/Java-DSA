3f92e938234a9f441ee7114a02ff387093df300e
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-21 23:55:38 +0530

    started linkedlist

---
+++ 17LinkedList/Java1_Basics.java
@@ -0,0 +1,25 @@
+public class Java1_Basics {
+    public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    public static Node head;
+    public static Node tail;
+    public static void main(String[] args) {
+        Java1_Basics ll=new Java1_Basics();
+        // ll.head=new Node(1);
+        // ll.head.next=new Node(2);
+
+        //Methods
+        //add()
+        //remove()
+        //print()
+        //search()
+        
+    }
+    
+}
