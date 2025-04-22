789a92f9ea14d1c24979c6a1579e3d7012b29dac
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-22 23:15:10 +0530

    add in linked list

--- 17LinkedList/Java1_Basics.java
+++ 17LinkedList/Java1_Basics.java
@@ -9,6 +9,22 @@ public class Java1_Basics {
     }
     public static Node head;
     public static Node tail;
+
+    //Add in Linked List
+    public void addFirst(int data){
+        //step-1: create new node
+        Node newNode=new Node(data);
+        if(head == null){
+            head=tail=newNode;
+            return;
+        }
+
+        //step-2: newNode next = head
+        newNode.next=head;  //link
+
+        //step-3: head = newNode
+        head=newNode;
+    }
     public static void main(String[] args) {
         Java1_Basics ll=new Java1_Basics();
         // ll.head=new Node(1);
@@ -19,6 +35,10 @@ public class Java1_Basics {
         //remove()
         //print()
         //search()
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+
         
     }
     
