5d1ca690faa5c16fd0cc7047e1b32f432706f0e1
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-04-26 23:33:54 +0530

    remove from ll

--- 17LinkedList/Java1_Basics.java
+++ 17LinkedList/Java1_Basics.java
@@ -1,86 +1,115 @@
-import java.security.PublicKey;
-
 public class Java1_Basics {
     public static class Node{
         int data;
         Node next;
         public Node(int data){
-            this.data=data;
-            this.next=null;
+            this.data = data;
+            this.next = null;
         }
     }
+
     public static Node head;
     public static Node tail;
     public static int size;
 
-    //Add in Linked List
+    // Add in Linked List (at the beginning)
     public void addFirst(int data){
-        //step-1: create new node
-        Node newNode=new Node(data);
+        // step-1: create new node
+        Node newNode = new Node(data);
         size++;
+        
         if(head == null){
-            head=tail=newNode;
+            head = tail = newNode;
             return;
         }
 
-        //step-2: newNode next = head
-        newNode.next=head;  //link
+        // step-2: newNode next = head
+        newNode.next = head;  // link
 
-        //step-3: head = newNode
-        head=newNode;
+        // step-3: head = newNode
+        head = newNode;
     }
      
-     //Add in Last in Linked List
+    // Add in Last in Linked List
     public void addLast(int data){
-        Node newNode=new Node(data);
+        Node newNode = new Node(data);
         size++;
-        if(head==null){
-            head=tail=newNode;
+        
+        if(head == null){
+            head = tail = newNode;
             return;
         }
-        tail.next=newNode;
-        tail=newNode;
+        
+        tail.next = newNode;
+        tail = newNode;
     }
 
-        // Print a linked list
-        public void print(){
-            if(head==null){
-                System.out.println("LL is empty");
-                return;
-            }
-            Node temp=head;
-            while (temp != null){
-                System.out.print(temp.data+"->");
-                temp=temp.next;
-            }
-            System.out.println("null");
+    // Print a linked list
+    public void print(){
+        if(head == null){
+            System.out.println("LL is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
     }
 
-    //Add in the middle
+    // Add in the middle (at specific index)
     public void add(int idx, int data){
-        Node newNode=new Node(data);
-        Node temp=head;
-        int i=0;
-        while(i<idx-1){
-            temp=temp.next;
+        if(idx < 0 || idx > size){
+            System.out.println("Invalid Index");
+            return;
+        }
+        
+        Node newNode = new Node(data);
+        Node temp = head;
+        
+        if(idx == 0){
+            addFirst(data);  // If index is 0, call addFirst
+            return;
+        }
+
+        int i = 0;
+        while(i < idx - 1){
+            temp = temp.next;
             i++;
         }
 
-        //i=idx-1;temp->prev
-        newNode next=temp.next;
-        temp.next=newNode;
-
+        // i = idx - 1; temp->prev (for doubly ll)
+        newNode.next = temp.next;
+        temp.next = newNode;
+        size++;
     }
-    public static void main(String[] args) {
-        Java1_Basics ll=new Java1_Basics();
-        // ll.head=new Node(1);
-        // ll.head.next=new Node(2);
+     // Remove from linked list
+    public int removeFirst(){
+        if(size==0){
+            System.out.println("LL is Empty");
+            return Integer.MIN_VALUE;
+        }
+        else if(size==1){
+            int val=head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        int val=head.data;
+        head=head.next;
+        size--;
+        return val;
+    }
 
-        //Methods
-        //add()
-        //remove()
-        //print()
-        //search()
+    public static void main(String[] args) {
+        Java1_Basics ll = new Java1_Basics();
+
+        // Methods
+        // addFirst()
+        // addLast()
+        // print()
+        // add()
 
         ll.print();
         ll.addFirst(2);
@@ -92,10 +121,10 @@ public class Java1_Basics {
         ll.addFirst(4);
         ll.print();
 
-        ll.add(2,9);
+        ll.add(2, 9);  // Add 9 at index 2
         ll.print();
 
-        
+        ll.removeFirst();
+        ll.print();
     }
-    
 }
