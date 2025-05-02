d66c949a791cd48e3774687dcce6449a9fa6ec4f
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-05-02 23:58:28 +0530

    check palindrome

--- 17LinkedList/Java5_palindromeLL.java
+++ 17LinkedList/Java5_palindromeLL.java
@@ -23,6 +23,37 @@ public class Java5_palindromeLL {
        } 
        return slow; //slow is my midNode
     }
+
+    public boolean checkPalindrome(){
+        if(head==null || head.next != null){
+            return true;
+        }
+        //step1 - find mid
+        Node midNode = findMid(head);
+
+        //step2 - reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while(curr != null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        Node right=prev; //right half head
+        Node left=head;
+
+        //step3- check left half & right half
+        while(right != null){
+            if(left.data != right.data){
+                return false;
+            }
+            left =left.next;
+            right=right.next;
+        }
+        return true;
+    }
     public static void main(String[] args) {
         
     }
