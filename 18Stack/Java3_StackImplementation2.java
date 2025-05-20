17ca3f46a5604380cdfea296cf53039ec801a601
Madhu Solanki <122769446+Madhusolanki48@users.noreply.github.com>
2025-05-20 19:45:59 +0530

    stack implementation using JCF

---
+++ 18Stack/Java3_StackImplementation2.java
@@ -0,0 +1,19 @@
+import java.util.*;
+//Stack Implementation using Java Collections Framework
+public class Java3_StackImplementation2 {
+    public static void main(String[] args) {
+        //Stack s=new Stack();
+        Stack<Integer> s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+        
+    }
+    
+
+}
